43d50dbac8fe52fd747f04f416aea5ff4f9abf27
Aldrin Baroi <aldrin.baroi@gmail.com>
2020-10-15 13:04:39 -0700

    Updated to generate all config files. Fixed bugs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-CMD=$1
+source lib/dependency-manager
 
+CMD=$1
+MODULE=$2 
 
 # Create configuration files if the files don't exist 
 if [[ ! -f "${BROKER_CONF}" ]]; then
@@ -19,6 +21,18 @@ fi
 if [[ ! -f "${WEBSOCKET_CONF}" ]]; then
 	source /gen-websocket.conf 
 fi
+if [[ ! -f "${ZOOKEEPER_CONF}" ]]; then
+	source /gen-zookeeper.conf 
+fi
+if [[ ! -f "${GLOBAL_ZOOKEEPER_CONF}" ]]; then
+	source /gen-global_zookeeper.conf 
+fi
+if [[ ! -f "${BOOKKEEPER_CONF}" ]]; then
+	source /gen-bookkeeper.conf 
+fi
+if [[ ! -f "${STANDALONE_CONF}" ]]; then
+	source /gen-standalone.conf 
+fi 
 
 isZkServerReachable()
 {
@@ -38,10 +52,101 @@ isZkServerReachable()
 
 isBookKeeperServerReachable()
 {
-	:
+        local bkServer=$1
+        local hostAndPort=""
+        local host=""
+        local port=0
+        IFS=':' read -r -a hostAndPort <<< "$bkServer"
+        host=${hostAndPort[0]}
+        port=${hostAndPort[1]}
+        if (( $(nc -z $host $port > /dev/null 2>&1; echo $?) )); then
+                echo 0
+        else
+                echo 1
+        fi 
 }
 
-source $BK_SERVER_CONF > /dev/null 2>&1
+areServersReachable() {
+	local serverType=$1
+	local checkFunc=$2
+	local servers=$3	
+	local reachable=0
+	local serverList=${servers//,/ }
+	local chkCount
+	local server
+	if [[ -z "$serverType" ]]; then
+		abort "Server type not provided."
+	fi
+	if [[ -z "$checkFunc" ]]; then
+		abort "Check function is not provided."
+	fi
+	if [[ -z "$servers" ]]; then
+		abort "Servers list not provided."
+	fi
+	log "Checking if $serverType servers [$serverList] are reachable..."
+	for chkCount in $(seq 1 5); do
+		for server in $serverList; do
+			log "  * Checking $serverType server: $server..."
+			if (( $($checkFunc $server) )); then
+				reachable=1
+				break
+			fi
+			log "      $server not reachable."
+		done
+		if (( $reachable == 1 )); then
+			break
+		else
+			log "$serverType servers [$serverList] are not reachable. Checking again in 3 seconds..."
+			sleep 3 
+		fi
+	done
+	if (( $reachable == 0 )); then
+		abort "$serverType servers are not reachable. Tried 5 times.  Aborting..."
+	fi
+}
+
+initCluster()
+{
+	local CLUSTER_NAME=$1
+	local PULSAR_HOSTNAME=$2
+	if [[ -z "$CLUSTER_NAME" ]]; then
+		abort "Cluster name not provided."
+	fi
+	if [[ -z "$PULSAR_HOSTNAME" ]]; then
+		abort "Pulsar hostname not provided."
+	fi
+	INIT_CMD="initialize-cluster-metadata 
+			--cluster \"$CLUSTER_NAME\"
+			--zookeeper \"$zookeeperServers\" 
+			--configuration-store \"$configurationStoreServers\" 
+			--web-service-url \"http://$PULSAR_HOSTNAME:$webServicePort\"/ 
+			--broker-service-url \"pulsar://$PULSAR_HOSTNAME:$brokerServicePort/\" "
+	if [[ ! -z "webServicePortTls" ]]; then
+		$INIT_CMD+=" --web-service-url-tls \"https://$PULSAR_HOSTNAME:$webServicePortTls\"/"
+	fi
+	if [[ ! -z "webServicePortTls" ]]; then
+		$INIT_CMD=" --broker-service-url-tls \"pulsar+ssl://$PULSAR_HOSTNAME:$brokerServicePort/\""
+	fi
+	exec gosu pulsar pulsar $INIT_CMD
+	if (( $? )) ; then
+		log "Failed to initialize the cluster"
+	else
+		log "Successfully initialized the cluster"
+	fi
+} 
+
+declare -A CONF_FILES
+CONF_FILES['broker']=$BROKER_CONF
+CONF_FILES['bookie']=$BOOKKEEPER_CONF
+CONF_FILES['zookeeper']=$ZOOKEEPER_CONF
+CONF_FILES['global-zookeeper']=$GLOBAL_ZOOKEEPER_CONF
+CONF_FILES['discovery']=$DISCOVERY_CONF
+CONF_FILES['proxy']=$PROXY_CONF
+CONF_FILES['websocket']=$WEBSOCKET_CONF
+CONF_FILES['standalone']=$STANDALONE_CONF
+CONF_FILES['client']=$CLIENT_CONF
+
+loadPropertiesFile $BROKER_CONF 
 
 #
 # If the command is pulsar then check if 
@@ -49,35 +154,34 @@ source $BK_SERVER_CONF > /dev/null 2>&1
 #  2) bookkeeper nodes 
 # are reachable & if not then abort.  
 #
-if [[ "$CMD" == "pulsar" ]]; then
-	reachable=0
-	serverList=${zkServers//,/ }
-	echo "Checking if Zookeeper servers reachable..."
-	for chkCount in $(seq 1 5); do
-		for server in $serverList; do
-			echo "  * Checking Zookeeper server: $server..."
-			if (( $(isZkServerReachable $server) )); then
-				reachable=1
-				break
-			fi
-			echo "      $server not reachable."
-		done
-		if (( $reachable == 1 )); then
-			break
-		else
-			echo "Zookeeper servers are not reachable. Checking again in 3 seconds..."
-			sleep 3 
-		fi
-	done
-	if (( $reachable == 1 )); then
-		exec gosu pulsar "$@"
-	else
-		echo "Zookeeper servers are not reachable. Tried 5 times.  Aborting..."
-		exit 1
+if [[ "$CMD" == "pulsar" ]]; then 
+	if [[ -z "$MODULE" ]]; then
+		# default module
+		MODULE="broker"  
 	fi
+	areServersReachable "Local Zookeeper" isZkServerReachable "$zookeeperServers"
+	areServersReachable "Configuration Store" isZkServerReachable "$configurationStoreServers"
+	case "$MODULE" in
+		broker)
+			if [[ ! -z "${bookkeeperMetadataServiceUri}" ]]; then
+				bookkeeperZookeeperServers="$(echo ${bookkeeperMetadataServiceUri} | sed 's/.*[/][/]//; s/[/].*//; s/[;]/,/')"
+				areServersReachable "External Bookkeeper's Zookeeper" isZkServerReachable "$bookkeeperZookeeperServers"
+			fi
+			exec gosu pulsar pulsar broker
+			;;
+		*)
+			shift 1
+			loadPropertiesFile ${CONF_FILES[$MODULE]} 
+			exec gosu pulsar pulsar "$@"
+			;;
+	esac
+elif [[ "$CMD" == "initCluster" ]]; then
+	areServersReachable "Local Zookeeper" isZkServerReachable "$zookeeperServers"
+	areServersReachable "Configuration Store" isZkServerReachable "$configurationStoreServers"
+	shift 1
+	initCluster "$@" 
 else
 	exec gosu pulsar "$@"
-fi
-
+fi 
 
 #::END::
